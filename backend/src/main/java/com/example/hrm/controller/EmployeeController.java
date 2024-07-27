245cefbfba5855e7588fe670248c1f4bc08bc34b
root <root@master.home>
2024-07-27 13:41:41 +0200

    add new file

--- backend/src/main/java/com/example/hrm/controller/EmployeeController.java
+++ backend/src/main/java/com/example/hrm/controller/EmployeeController.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/employees")
